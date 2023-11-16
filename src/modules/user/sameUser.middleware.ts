b6033246272e9967810f3b625d1b32ce01a3e5ac
Aniekan Offiong <ekanoffiong@gmail.com>
2023-11-16 11:00:35 +0100

    Fixed validation for sameUserMiddleware

--- src/modules/user/sameUser.middleware.ts
+++ src/modules/user/sameUser.middleware.ts
@@ -6,7 +6,7 @@ import UserPermissionException from "./exception/userPermission.exception"
 export default (req: RequestWithUser, _res: Response, next: NextFunction) => {
     const user: User|undefined = req.user
     const { id }: { id?: number } = req.params;
-    if (user?.id !== id) {
+    if (user?.id !== Number(id)) {
         next(new UserPermissionException())
     } else {
         next()
